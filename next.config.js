e95e222d556028fc10e64c1d359dbef57f007e0d
Dimas Hermawan <dimas9c3@gmail.com>
2022-08-23 08:29:58 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    // disable: process.env.NODE_ENV === 'development',
     register: true,
-    skipWaiting: true,
+		skipWaiting: false,
+		runtimeCaching,
   },
 });
