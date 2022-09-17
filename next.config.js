9897374b96b2061bd52fec5455c8267d6d5bdfb9
Dimas Hermawan <dimas9c3@gmail.com>
2022-09-17 08:05:43 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-// const withPWA = require("next-pwa");
-// const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     // disable: process.env.NODE_ENV === 'development',
-//     register: true,
-// 		skipWaiting: false,
-// 		runtimeCaching,
-//     buildExcludes: [/middleware-manifest.json$/]
-//   },
-// });
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    // disable: process.env.NODE_ENV === 'development',
+    register: true,
+		skipWaiting: false,
+		runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  },
+});
